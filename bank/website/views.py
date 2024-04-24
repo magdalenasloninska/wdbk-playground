56e6969fdfbe75845d725654f7a77b30e6bc67cb
Magdalena Słonińska <magdalena732@gmail.com>
2024-04-24 23:28:58 +0200

    Separated 'New transfer' and 'History' tabs

--- bank/website/views.py
+++ bank/website/views.py
@@ -10,15 +10,28 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    return render_template("home.html", user=current_user)
+
+@views.route('/new-transfer', methods=['GET', 'POST'])
+@login_required
+def new_transfer():
     if request.method == 'POST':
         title = request.form.get('title')
+        amount = request.form.get('amount')
 
         if len(title) < 3:
             flash('Transfer title is too short!', category='error')
         else:
-            new_transfer = Transfer(title=title, sender=current_user.id)
+            new_transfer = Transfer(
+                    title=title,
+                    sender=current_user.id,
+                    amount=amount)
             db.session.add(new_transfer)
             db.session.commit()
-            flash('Transfer sent!', category='success')
 
-    return render_template("home.html", user=current_user)
+    return render_template("new_transfer.html", user=current_user)
+
+@views.route('/history', methods=['GET'])
+@login_required
+def history():
+    return render_template("history.html", user=current_user)
